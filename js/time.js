0934a8ac521b8ee2f168f3c7cdbba1fa2884b2d5
morinanami <2115083@venus.fujijoshi.ac.jp>
2025-01-29 11:07:33 +0900

    Update time.js

--- js/time.js
+++ js/time.js
@@ -32,3 +32,30 @@ text.classList.add('is-active');
 setInterval(() => {
   text.classList.toggle('is-active');
 }, 3000);
+
+
+// 以下は終わりましたよポップアップ(2025年1月21日追記)
+window.addEventListener('load', function() {
+  if(!sessionStorage.getItem('disp_popup')) {
+      sessionStorage.setItem('disp_popup', 'on');
+      const body = document.querySelector('body');
+      const bgPopup = document.querySelector('.bg_onetime_popup');
+      const popup = document.querySelector('.onetime_popup');
+      const popupTitleClose = document.querySelector('.onetime_popup_title_close');
+      body.classList.add('open_popup');
+
+      bgPopup.addEventListener('click', function() {
+          closePopup();
+      });
+      popup.addEventListener('click', function(e) {
+          e.stopPropagation();
+      });
+      popupTitleClose.addEventListener('click', function() {
+          closePopup();
+      });
+
+      function closePopup() {
+          body.classList.remove('open_popup');
+      }
+  }
+}, false);
